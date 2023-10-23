17fc8f66ad40d18daf5aa00467b1a65f6692235b
Sharmin64 <lizaeatsbanana@gmail.com>
2023-10-23 22:44:40 +0600

    import component in layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,24 @@
-import React from "react";
+import {Sora} from "@next/font/google";
 
-const Layout = () => {
-  return <div>Layout</div>;
+//*component
+import Nav from "./Nav";
+import Header from "./Header";
+import TopLeftImg from "../components/TopLeftImg";
+
+const sora = Sora({
+  subsets: ["latin"],
+  variables: "--font-sora",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
+
+const Layout = ({children}) => {
+  return (
+    <div>
+      <TopLeftImg />
+      <Nav />
+      <Header />
+    </div>
+  );
 };
 
 export default Layout;
