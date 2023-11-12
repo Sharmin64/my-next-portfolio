358b9be9a8e534f1ac9763d1205dc583160f9b1f
Sharmin64 <lizaeatsbanana@gmail.com>
2023-11-12 22:47:31 +0600

    style updated

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,41 @@
+//next image
+import Image from "next/image";
+
+//*component
+import ParticlesContainer from "../components/ParticlesContainer";
+import ProjectsBtn from "../components/ProjectsBtn";
+import Avatar from "../components/Avatar";
+
+//*Framer motion
+import {motion} from "framer-motion";
+
+// *variants
+import {fadeIn} from "../variants";
+
 const Home = () => {
-  return <div>Home this is sweet home albama</div>;
+  return (
+    <div className="bg-primary/60 h-full">
+      {/* text */}
+      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+          {/* title  */}
+          <h1 className="h1">
+            Transforming Ideas <br /> Into{" "}
+            <span className="text-accent">Digital Reality</span>
+          </h1>
+          {/* subtitle */}
+          <p className="max-x-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam,
+            eius optio labore animi asperiores itaque libero veritatis sapiente
+            saepe sed.
+          </p>
+        </div>
+      </div>
+
+      {/* image  */}
+      <div>image</div>
+    </div>
+  );
 };
 
 export default Home;
