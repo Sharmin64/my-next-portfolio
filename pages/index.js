c98e3680e3e28cb357ad05e7da3acb7a753aeb05
Sharmin64 <lizaeatsbanana@gmail.com>
2023-11-29 23:28:30 +0600

    particles updated

--- pages/index.js
+++ pages/index.js
@@ -62,7 +62,18 @@ const Home = () => {
         {/* bg image  */}
         <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
         {/* particles */}
-        <div></div>
+        <ParticlesContainer />
+        {/* avater image */}
+        <motion.div
+          variants={fadeIn("up", 0.5)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="w-full h-full max-w-[637px] max-h-[578px] absolute -bottom-22 right-0 mix-blend-plus-lighter drop-shadow-md lg:bottom-16 lg:right-[6%]"
+        >
+          <Avatar />
+        </motion.div>
       </div>
     </div>
   );
