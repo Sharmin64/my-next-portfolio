5524908a88f68b3b792fef95e0330df61fd692df
Sharmin64 <lizaeatsbanana@gmail.com>
2023-12-29 00:18:46 +0600

    slider updated

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -61,8 +61,32 @@ const ServiceSlider = () => {
           spaceBetween: 15,
         },
       }}
+      freeMode={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={{ FreeMode, Pagination }}
+      className="h-[240px] sm:h-[340px]"
     >
-      Service Slider
+      {serviceData.map((item, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div className="bg-[rgba(65,47, 123, 0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300">
+              {/* icon */}
+              <div className="text-4xl text-accent mb-4">{item.icon}</div>
+              {/* title & desc  */}
+              <div>
+                <div>{item.title}</div>
+                <p>{item.description}</p>
+              </div>
+              {/* arrow */}
+              <div className="text-3xl">
+                <RxArrowTopRight />
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
     </Swiper>
   );
 };
