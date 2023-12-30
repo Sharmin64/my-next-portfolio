0a0268261667d52cc3f673de759c8953ba15d2e7
Sharmin64 <lizaeatsbanana@gmail.com>
2023-12-30 23:13:15 +0600

    motion rendered

--- pages/services/index.js
+++ pages/services/index.js
@@ -15,9 +15,15 @@ const Services = () => {
         <div className="flex flex-col xl:flex-row gap-x-8">
           {/* texxt  */}
           <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
-            <h2 className="h2 xl:mt-8">
+            <motion.h2
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h2 xl:mt-8"
+            >
               My Services <span className="text-accent">.</span>
-            </h2>
+            </motion.h2>
             <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit.
               Voluptatibus atque illum sunt culpa facilis inventore.
