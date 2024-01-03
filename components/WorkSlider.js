61f9451449371a2812d90eb9833d51cd5582cd63
Sharmin64 <lizaeatsbanana@gmail.com>
2024-01-03 23:28:30 +0600

    workslider updated

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -70,26 +70,40 @@ const WorkSlider = () => {
       className="h-[280px] sm:h-[480px]"
     >
       {workSlides.slides.map((slide, index) => {
-        return;
-        <SwiperSlide key={index}>
-          <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
-            {slide.images.map((image, index) => {
-              return (
-                <div key={index}>
-                  <div>
-                    {/* images */}
-                    <Image
-                      src={image.path}
-                      width={500}
-                      height={300}
-                      alt="image"
-                    />
+        return (
+          <SwiperSlide key={index}>
+            <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
+              {slide.images.map((image, index) => {
+                return (
+                  <div
+                    className="relative rounded-lg overflow-hidden flex items-center justify-center group"
+                    key={index}
+                  >
+                    <div className="flex items-center justify-center relative overflow-hidden">
+                      {/* images */}
+                      <Image
+                        src={image.path}
+                        width={500}
+                        height={300}
+                        alt="image"
+                      />
+                      <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
+                      {/* title */}
+                      <div>
+                        <div>
+                          {/* title part 1 */}
+                          <div>title part one</div>
+                          {/* title part 1 */}
+                          <div>title part one</div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              );
-            })}
-          </div>
-        </SwiperSlide>;
+                );
+              })}
+            </div>
+          </SwiperSlide>
+        );
       })}
     </Swiper>
   );
